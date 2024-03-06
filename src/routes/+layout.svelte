6adb7ec4ff9f92e404c0119e203a5bc67708886b
Kwan Kit Chung <kwankit0629@gmail.com>
2024-03-07 01:29:11 +0800

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+  import "../app.pcss";
+  import "../app.pcss";
+  import "../app.pcss";
+</script>
+
+<nav>
+  <a href="/">Home</a>
+  <a href="/about">About</a>
+  <a href="/lists">Lists</a>
+</nav>
+
+<slot></slot>
